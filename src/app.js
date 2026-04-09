0f02a3ec82ff515d13871e86678cb5d2efe49cc2
jonah <mudzingwajonah@gmail.com>
2026-04-09 16:57:50 +0200

    feat : ammended configuraton to use host

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import express from "express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "0.0.0.0";
 
 app.get("/", (req, res) => {
   res.send("Dzidzo backend running 🚀");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server running on http://${HOST}:${PORT}`);
 });
